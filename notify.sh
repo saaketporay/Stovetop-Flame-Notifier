74bc5a55ee558848d650e21469383af9a44aee6a
Saaket Poray <saaketporay@gmail.com>
2016-08-07 08:37:06 -0500

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN="o.4j4VmTh9uEqJbcVfwNzXK2aKyM4B1BC3"
+TOKEN="Your TOKEN here"
 TITLE="$1"
 BODY="$2"
 curl -u $TOKEN: https://api.pushbullet.com/v2/pushes -d type=note -d title="$TITLE" -d body="$BODY"
